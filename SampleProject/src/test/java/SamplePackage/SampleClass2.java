146a63c2cb5810a53522466e2e8d4bc2e493aaa6
Tarique <Tarique@Tarique-PC>
2017-11-05 20:27:46 +0530

    partial changes

--- SampleProject/src/test/java/SamplePackage/SampleClass2.java
+++ SampleProject/src/test/java/SamplePackage/SampleClass2.java
@@ -10,5 +10,15 @@ public class SampleClass2 {
 	{
 		System.out.println("This is test4..");
 	}
+	
+	@Test
+	public void test5()
+	{
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.println("This is for loop...");
+			
+		}
+	}
 
 }
